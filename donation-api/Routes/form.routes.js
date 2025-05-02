2e3e77deaab4295b8b8d72634717770b5b731dc6
Banti Singh <testingvisitor896@gmail.com>
2025-05-02 15:05:14 +0530

    route and master form setup

--- donation-api/Routes/form.routes.js
+++ donation-api/Routes/form.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { getFormConfigByRoute } = require('../controllers/form.controller');
 const router = express.Router();
   
-router.get('/master/donation-type', getFormConfigByRoute);
+router.get('/master', getFormConfigByRoute);
  
 module.exports = router;
